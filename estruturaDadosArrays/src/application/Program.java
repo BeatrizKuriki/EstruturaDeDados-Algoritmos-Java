16dbdf4bf00a9c49e9106a996fda3aa0bab34539
BeatrizKuriki <b.f.kuriki@gmail.com>
2022-10-29 10:15:25 -0300

    iterando o vetor e método toString

--- estruturaDadosArrays/src/application/Program.java
+++ estruturaDadosArrays/src/application/Program.java
@@ -11,12 +11,24 @@ public class Program {
 		 * UM VETOR ARMAZENA UMA SEQUÊNCIA DE VALORES ONDE TODOS SÃO DO MESMO TIPO;
 		 */
 
-		Vetor vetor = new Vetor(2);
+		Vetor vetor = new Vetor(10);
 
 
 		vetor.adiciona("elemento 1");
 		vetor.adiciona("elemento 2");
 		vetor.adiciona("elemento 3");
+		
+		System.out.println(vetor.tamanho());
+		
+		/*
+		 * QUANDO FOR IMPRIMIR O VETOR SERÁ GERADO UM ARRAY COM OS TRÊS ELEMENTOS
+		 * QUE FORAM ADICIONADOS E MAIS OS ESPAÇOS VAZIOS. NO ENTANTO QUEREMOS QUE O VETOR
+		 * SÓ MOSTRE OS ESPAÇOS COM ELEMENTOS E NÃO COM NULL.
+		 * ENTÃO DENTRO DO MÉTODO TOSTRING SERÁ NECESSÁRIO ITERAR UTILIZANDO O LAÇO FOR TODOS OS ELEMENTOS DO VETOR E 
+		 * ADICIONAR A STRING
+		 * 
+		 */
+		System.out.println(vetor);
 
 
 
--- estruturaDadosArrays/src/entities/Vetor.java
+++ estruturaDadosArrays/src/entities/Vetor.java
@@ -1,5 +1,7 @@
 package entities;
 
+import java.util.Arrays;
+
 public class Vetor {
 
 	private String[] elementos;
@@ -41,6 +43,29 @@ public class Vetor {
 			return true;
 			}
 		return false;
+	}
+	public int tamanho() {
+		return this.tamanho;
+	}
+	
+	@Override
+	public String toString() {
+		
+		StringBuilder s = new StringBuilder();
+		s.append("[");
+		
+		for(int i = 0; i< this.tamanho-1; i++) {
+			s.append(this.elementos[i]);
+			s.append(", ");
+		}
+		
+		if(this.tamanho>0) {
+			s.append(this.elementos[this.tamanho-1]);
+		}
+		s.append("]");
+		
+		
+		return s.toString();
 	}
 
 }
