8fccb93cbcfb744545b8a1f5b3ca9f2bb00a1622
BeatrizKuriki <b.f.kuriki@gmail.com>
2022-10-29 09:43:09 -0300

    criação de novo método add do tipo boolean

--- estruturaDadosArrays/src/application/Program.java
+++ estruturaDadosArrays/src/application/Program.java
@@ -5,26 +5,24 @@ import entities.Vetor;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		/*
 		 * UM VETOR É A ESTRUTURA DE DADOS MAIS SIMPLES QUE EXISTE!
 		 * UM VETOR ARMAZENA UMA SEQUÊNCIA DE VALORES ONDE TODOS SÃO DO MESMO TIPO;
 		 */
-		
+
 		Vetor vetor = new Vetor(2);
-		
-		try {
-			vetor.adiciona("elemento 1");
-			vetor.adiciona("elemento 2");
-			vetor.adiciona("elemento 3");
-			
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		
-		
-	
+
+
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		vetor.adiciona("elemento 3");
+
+
+
+
+
+
 	}
 
 }
--- estruturaDadosArrays/src/entities/Vetor.java
+++ estruturaDadosArrays/src/entities/Vetor.java
@@ -1,44 +1,46 @@
 package entities;
 
 public class Vetor {
-	
-	private String [] elementos;
+
+	private String[] elementos;
 	private int tamanho;
-	
-	public Vetor( int capacidade) {
+
+	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
-		this.tamanho =0;
+		this.tamanho = 0;
 	}
 
-	
 	/*
 	 * O VALOR INICIAL DE UMA STRING QUE NÃO FOI ATRIBUÍDO É POR DEFAULT == NULL
-	 * INTEIRO == 0;
-	 * BOOLEAN == FALSE;
+	 * INTEIRO == 0; BOOLEAN == FALSE;
 	 */
-	
-	/*public void adiciona(String elemento) {
-		for(int i =0; i < this.elementos.length; i++) {
-			if(this.elementos[i] == null) {
-				this.elementos[i] = elemento;
-				break;
-			}
-		}
-		
-		
-	}*/
-	
-	public void adiciona(String elemento) throws Exception{
-		if(this.tamanho < this.elementos.length) {
+
+	/*
+	 * public void adiciona(String elemento) { for(int i =0; i <
+	 * this.elementos.length; i++) { if(this.elementos[i] == null) {
+	 * this.elementos[i] = elemento; break; } }
+	 * 
+	 * 
+	 * }
+	 */
+
+	/*
+	 * public void adiciona(String elemento) throws Exception{ if(this.tamanho <
+	 * this.elementos.length) { this.elementos[this.tamanho] = elemento;
+	 * this.tamanho++;
+	 * 
+	 * }else { throw new Exception
+	 * ("Vetor atingiu a capacidade máxima não é possível adicionar mais elementos! "
+	 * ); }
+	 */
+
+	public boolean adiciona(String elemento){
+		if (this.tamanho < this.elementos.length) {
 			this.elementos[this.tamanho] = elemento;
 			this.tamanho++;
-			
-		}else {
-			throw new Exception ("Vetor atingiu a capacidade máxima não é possível adicionar mais elementos! ");
-		}
-		
-		
+			return true;
+			}
+		return false;
 	}
-	
 
 }
