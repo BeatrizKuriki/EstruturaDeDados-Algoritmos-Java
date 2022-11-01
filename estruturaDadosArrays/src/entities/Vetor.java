5d46a760044f4140f563fa582160e1f5d46db2e7
BeatrizKuriki <b.f.kuriki@gmail.com>
2022-11-01 15:56:56 -0300

    método para aumentar capacidade do vetor

--- estruturaDadosArrays/src/entities/Vetor.java
+++ estruturaDadosArrays/src/entities/Vetor.java
@@ -33,8 +33,20 @@ public class Vetor {
 	 * ("Vetor atingiu a capacidade máxima não é possível adicionar mais elementos! "
 	 * ); }
 	 */
+	
+	private void aumentaCapacidade() {
+		if(tamanho == this.elementos.length) {
+			String[] elementosNovos = new String[this.elementos.length * 2];
+			for(int i=0; i< this.elementos.length; i++) {
+				elementosNovos[i] = this.elementos[i];				
+			}
+			this.elementos = elementosNovos;
+		}
+		
+	}
 
 	public boolean adiciona(String elemento){
+		this.aumentaCapacidade();
 		if (this.tamanho < this.elementos.length) {
 			this.elementos[this.tamanho] = elemento;
 			this.tamanho++;
@@ -46,6 +58,7 @@ public class Vetor {
 	//0 1 2 3 4 5 6 = TAMANHO É 5
 	//B C E F G + +
 	public boolean adiciona(int posicao, String elemento){
+		this.aumentaCapacidade();
 		 if(!(posicao >= 0 && posicao < tamanho)) {
 			throw new IllegalArgumentException("POSIÇÃO INVÁLIDA!");	
 			}
