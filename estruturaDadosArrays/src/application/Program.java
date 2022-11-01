49d35a1e9053b46fd153c55ca1e088e0b6a068a2
BeatrizKuriki <b.f.kuriki@gmail.com>
2022-11-01 16:10:11 -0300

    método para remover elementos no vetor

--- estruturaDadosArrays/src/application/Program.java
+++ estruturaDadosArrays/src/application/Program.java
@@ -11,13 +11,17 @@ public class Program {
 		 * UM VETOR ARMAZENA UMA SEQUÊNCIA DE VALORES ONDE TODOS SÃO DO MESMO TIPO;
 		 */
 
-		Vetor vetor = new Vetor(10);
+		Vetor vetor = new Vetor(5);
 
 
 		vetor.adiciona("elemento 1");
 		vetor.adiciona("elemento 2");
 		vetor.adiciona("elemento 3");
 		vetor.adiciona("elemento 4");
+		vetor.adiciona("elemento 5");
+		vetor.adiciona("elemento 5");
+		vetor.adiciona("elemento 6");
+		vetor.adiciona("elemento 7");
 		
 		System.out.println(vetor.tamanho());
 		
@@ -32,7 +36,7 @@ public class Program {
 		System.out.println(vetor);
 		
 	System.out.println(vetor.busca(1));	
-
+System.out.println("*******************************************************");
 
 
 
--- estruturaDadosArrays/src/entities/Vetor.java
+++ estruturaDadosArrays/src/entities/Vetor.java
@@ -98,6 +98,24 @@ public class Vetor {
 		return -1;
 			
 	}
+	//B G D E F -> posição a ser removida é 1 (G)
+	//0 1 2 3 4  -> tamanho é 5
+	//vetor [1] = vetor[2]
+	//vetor[2] = vetor[3]
+	//vetor[3] = vetor[4]
+	public void removeElemento(int posicao) {
+		if(!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("POSIÇÃO INVÁLIDA!");
+			
+		}
+		for(int i = posicao; i < this.tamanho-1; i++){
+			this.elementos[i] = this.elementos[i+1];				
+		}
+		this.tamanho--;
+		
+		
+	}
+
 	
 
 	
