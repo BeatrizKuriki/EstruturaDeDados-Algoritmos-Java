aa18214a6c0cc00bce6d8826d1c6d516ac895358
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-11-07 09:29:23 -0300

    trabalhando com a classe arrayList do java

---
+++ estruturaDadosArrays/src/application/Exerc12.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.util.ArrayList;
+
+public class Exerc12 {
+
+	public static void main(String[] args) {
+		ArrayList<String> arrayList = new ArrayList<String>();
+		
+		arrayList.add("A");
+		arrayList.add("B");
+		
+		System.out.println(arrayList);
+
+	}
+
+}
