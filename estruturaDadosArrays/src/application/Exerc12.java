9ff126549c2996f868f68fec0c02e756fb7e8939
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-11-07 09:48:39 -0300

    comparando as classes vetor

--- estruturaDadosArrays/src/application/Exerc12.java
+++ estruturaDadosArrays/src/application/Exerc12.java
@@ -11,6 +11,21 @@ public class Exerc12 {
 		arrayList.add("B");
 		
 		System.out.println(arrayList);
+		
+		arrayList.add(0, "1");
+		System.out.println(arrayList);
+		
+		//o método contains é similar com o método de busca que implementamos no vetor
+		boolean existe = arrayList.contains("A");
+		if(existe) {
+			System.out.println("Elemento existe no array!");
+		}else {
+			System.out.println("Elemento não existe no array!");
+		}
+		
+		int pos = arrayList.indexOf("B");
+		
+		System.out.println("O elemento B encontra-se na posição: "+ pos);
 
 	}
 
