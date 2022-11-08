ca5dfcc26f9dad989078196adba78dc585cc8568
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-11-08 11:15:51 -0300

    fazendo um classe genérica para as demais herdarem

--- pilhas/src/entities/Pilha.java
+++ pilhas/src/entities/Pilha.java
@@ -1,30 +1,63 @@
 package entities;
 
-
 /*
  * PILHAS SEGUEM O COMPORTAMENTO LIFO
  * LAST IN
  * FIRST OUT
  */
-public class Pilha <T> {
+public class Pilha<T> {
 	private T[] elementos;
 	private int tamanho;
-	
+
 	public Pilha(int capacidade) {
 		elementos = (T[]) new Object[capacidade];
-		tamanho =0;
-	}
-	
-	public int tamanho() {
-		return this.tamanho;
-		
+		tamanho = 0;
 	}
 
-	public String toString() {
-		
+	
+	protected void remove(int posicao) {
+		if (!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("Posicao inválida");
+		}
+		for (int i = posicao; i < tamanho - 1; i++) {
+			elementos[i] = elementos[i + 1];
+		}
+		tamanho--;
 	}
-	
-	
-	
+
+	@SuppressWarnings("unchecked")
+	private void aumentaCapacidade() {
+		if (this.tamanho == this.elementos.length) {
+			T[] elementosNovos = (T[]) new Object[this.elementos.length * 2];
+			for (int i = 0; i < this.elementos.length; i++) {
+				elementosNovos[i] = this.elementos[i];
+			}
+			this.elementos = elementosNovos;
+		}
+	}
+
+	public int tamanho() {
+		return this.tamanho;
+	}
+
+	@Override
+	public String toString() {
+
+		StringBuilder s = new StringBuilder();
+		s.append("[");
+
+		for (int i = 0; i < this.tamanho - 1; i++) {
+			s.append(this.elementos[i]);
+			s.append(", ");
+		}
+
+		if (this.tamanho > 0) {
+			s.append(this.elementos[this.tamanho - 1]);
+		}
+
+		s.append("]");
+
+		return s.toString();
+	}
 
 }
